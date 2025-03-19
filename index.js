135cb143117d326c5466837bac6792be8ac514cf
amird55 <amir.dgn@outlook.com>
2025-03-19 17:49:42 +0200

    מתחילים

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 //npm i express body-parser
+const port = 7777;
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+const bodyParser = require('body-parser');
+const path = require("path");
+app.use(bodyParser.urlencoded({extended: false}));
