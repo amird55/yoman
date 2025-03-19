bd832a5379763f431f13116b708bf51bd5212ea2
amird55 <amir.dgn@outlook.com>
2025-03-19 18:46:57 +0200

    שרת מופעל, עדיין בלי לעשות משהו

--- index.js
+++ index.js
@@ -7,3 +7,10 @@ app.use(express.json());
 const bodyParser = require('body-parser');
 const path = require("path");
 app.use(bodyParser.urlencoded({extended: false}));
+
+
+
+
+app.listen(port, () => {
+    console.log(`Now listening on port http://localhost:${port}`);
+});
