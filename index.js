5665ddf7f48ab6375c6addc7efaffd8c165af549
amird55 <amir.dgn@outlook.com>
2025-04-02 17:17:43 +0300

    עמוד EJS ראשון ובסיסי וגם בסיס הנתונים

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ app.set("view engine", "ejs");
 app.set('views', path.join(__dirname, "./views"));
 
 app.get('/', (req, res) => {
-    // res.sendFile(path.join(__dirname, 'index.html'));
     res.render("index", {});
 })
 
