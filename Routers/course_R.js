182b7e2d0f48c5bf0fa9b1ff8a0aead79f7123f6
amird55 <amir.dgn@outlook.com>
2025-04-09 23:31:35 +0300

    יצירת עמוד עריכת קורס (רק הצגה)

--- Routers/course_R.js
+++ Routers/course_R.js
@@ -8,7 +8,15 @@ router.get("/Add",(req,res)=>{
     res.render("crs_add",{});
 });
 router.post("/Add", [course_Mid.AddCourse], (req, res) => {
-    res.send("ok");
+    res.redirect("/Crs/List");
+});
+router.get("/Edit/:id",[course_Mid.GetOneCourse],(req,res)=>{
+    res.render("crs_add",{
+        data : req.one_course_data,
+    });
+});
+router.post("/Edit", [course_Mid.AddCourse], (req, res) => {
+    res.redirect("/Crs/List");
 });
 router.get("/List",[course_Mid.GetAllCourses],(req,res)=>{
     res.render("crs_list",{
@@ -16,3 +24,6 @@ router.get("/List",[course_Mid.GetAllCourses],(req,res)=>{
         courses : req.courses_data,
     });
 });
+router.post("/Delete",[course_Mid.DeleteCourse],(req,res)=>{
+    res.redirect("/Crs/List");
+})
--- middleware/course_Mid.js
+++ middleware/course_Mid.js
@@ -25,10 +25,42 @@ async function GetAllCourses(req,res,next){
 
     next();
 }
+async function GetOneCourse(req,res,next){
+    let id = parseInt(req.params.id);
+    let Query=`SELECT * FROM courses  WHERE id='${id}' `;
+    const promisePool = db_pool.promise();
+    let rows=[];
+    req.one_course_data=[];
+    try {
+        [rows] = await promisePool.query(Query);
+        if(rows.length > 0) {
+            req.one_course_data = rows[0];
+        }
+    } catch (err) {
+        console.log(err);
+    }
+
+    next();
+}
 async function DeleteCourse(req,res,next){
+    let id = parseInt(req.body.id);
+    if(id > 0) {
+        let Query = `DELETE FROM courses WHERE id='${id}' `;
+        const promisePool = db_pool.promise();
+        let rows = [];
+        try {
+            [rows] = await promisePool.query(Query);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    next();
 
 }
 module.exports = {
     AddCourse,
     GetAllCourses,
+    GetOneCourse,
+    DeleteCourse,
 }
