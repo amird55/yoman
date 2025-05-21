85c8129dc0e1d5688b33c429331bcf368a4ead63
amird55 <amir.dgn@outlook.com>
2025-05-21 18:50:35 +0300

    התקנת ספרייה לעבודה עם עוגיות

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//npm i express body-parser ejs mysql2 slashes@2.0.0 md5
+//npm i express body-parser ejs mysql2 slashes@2.0.0 md5 cookie-parser
 const port = 7777;
 const express = require('express');
 const app = express();
@@ -7,6 +7,9 @@ app.use(express.json());
 const bodyParser = require('body-parser');
 const path = require("path");
 app.use(bodyParser.urlencoded({extended: false}));
+
+var cookieParser = require('cookie-parser');
+app.use(cookieParser());
 
 let db_M = require('./database');
 global.db_pool = db_M.pool;
